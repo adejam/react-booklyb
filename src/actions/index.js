c4711062ca0d66995263468e38f7f2f842554b3f
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2020-09-14 23:25:32 +0100

    feat: Add method to dynamically create unique id for books

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,3 @@
-// const removeBookAction = id => ({ type: 'REMOVE_BOOK', id });
-// const createBookAction = id => ({ type: 'CREATE_BOOK', id });
 const actions = {
   removeBookAction: id => ({ type: 'REMOVE_BOOK', id }),
   createBookAction: id => ({ type: 'CREATE_BOOK', id }),
--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,8 +1,8 @@
 const initialState = {
   books: [
-    { id: 1, title: 'Oliver Twist', category: 'Fiction' },
-    { id: 2, title: 'Peter Pan', category: 'novel' },
-    { id: 3, title: 'Superman', category: 'Sci-fi' },
+    { id: Math.random(), title: 'Oliver Twist', category: 'Fiction' },
+    { id: Math.random(), title: 'Peter Pan', category: 'novel' },
+    { id: Math.random(), title: 'Superman', category: 'Sci-fi' },
   ],
 };
 
