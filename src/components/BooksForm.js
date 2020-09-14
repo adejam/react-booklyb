2c7c3f6418e3db2f53583df8d60149cbe80dff00
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2020-09-14 15:26:31 +0100

    feat: Add form with styling

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
-const BooksForm = () => <div />;
+const BooksForm = () => (
+  <form>
+    <div className="formGroup">
+      <input type="text" className="formControl" id="title" placeholder="Title" />
+    </div>
+    <div className="formGroup">
+      <select className="formControl" id="category">
+        <option value="">Choose Category</option>
+        <option value="Action">Action</option>
+        <option value="Biography">Biography</option>
+        <option value="History">History</option>
+        <option value="Horror">Horror</option>
+        <option value="Kids">Kids</option>
+        <option value="Learning">Learning</option>
+        <option value="Sci-Fi">Sci-Fi</option>
+      </select>
+    </div>
+    <div className="formGroup">
+      <button type="submit" className="btn btnPrimary">
+        Submit
+      </button>
+    </div>
+  </form>
+);
 
 export default BooksForm;
