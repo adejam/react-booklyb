1f296fbf5b1813eefa08e0176082b171f520080d
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2020-09-16 23:22:20 +0100

    fix: fix conflicts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,6 @@
 const actions = {
-<<<<<<< HEAD
   removeBookAction: payload => ({ type: 'REMOVE_BOOK', payload }),
   createBookAction: payload => ({ type: 'CREATE_BOOK', payload }),
-=======
-  removeBookAction: id => ({ type: 'REMOVE_BOOK', id }),
-  createBookAction: book => ({ type: 'CREATE_BOOK', book }),
->>>>>>> 4a9d821... feat: Add functionality to Add Book to store
 };
 
 export default actions;
