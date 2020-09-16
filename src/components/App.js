662f836d5d8a0a3a4ecd9e00b1a75835079a4806
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2020-09-16 23:44:57 +0100

    feat: Add category filter component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BooksList from './BooksList';
 import BooksForm from '../containers/BooksForm';
+import CategoryFilter from '../containers/CategoryFilter';
 
 function App() {
   return (
     <div className="App">
       <BooksList />
       <BooksForm />
+      <CategoryFilter />
     </div>
   );
 }
