ab1152438feec4d0e1d44770637ec82f350e612a
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2020-09-19 08:20:05 +0100

    feat: Add header

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Navbar = () => (
+  <header className="bgWhite">
+    <nav className="navbar" id="myTopnav">
+      <a className="navbarBrand azure" href="/">
+        Book Store
+      </a>
+      <nav>
+        <a href="#home" className="navItem">
+          Books
+        </a>
+        <a href="#news" className="navItem">
+          Category
+        </a>
+      </nav>
+      <nav className="mlAuto">
+        <a href="/avatar" className="avatar  navItem">
+          <img src="" width="15" height="15" alt="A" />
+        </a>
+      </nav>
+    </nav>
+  </header>
+);
+
+export default Navbar;
